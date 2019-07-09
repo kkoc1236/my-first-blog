f6e06098b5a75e70be6fa927bd0ef0262fc04d5e
Your Name <you@example.com>
2019-07-09 16:56:04 +0900

    etailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.shortcuts import render, get_object_or_404
 urlpatterns = [
     path('', views.gps, name='gps'),
+    path('blog/post_list.html', views.gps, name='gps'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.http import HttpResponseRedirect
 import re
 
 def gps(request):
-    if request.method == 'POST':
-        converted =  {'gpsinfo': gpsinfo}
-        return render(converted, 'blog/post_list.html', {'gpsinfo;'})
+    if request.method == 'post':
+        converted1 = 'Hello'
+        return render(request, 'blog/post_list.html', {'converted1' : converted1})
     else:
-        return render(request, 'blog/post_list.html', {})
+        converted = 'World'
+        return render(request, 'blog/post_list.html', {'convert' : converted})
