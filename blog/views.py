1243b465fb5d7a414265d9df80981ac8bdc09945
kim kwansub <kkoc1236@naver.com>
2019-07-11 23:51:43 +0900

    gpsconverter2.1

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from .models import Post
-from django.utils import timezone
 import re
 
 
@@ -23,17 +21,13 @@ def new_page(request):
     listlize = fulltextarea.split('\r\n')
     listlize2 = [x for x in listlize if x]
 
-    # pattern model
-    p_num = re.compile('\d')
-    p_hi = re.compile('-')
-    p_dot = re.compile('.')
-    p_ch = re.compile('[a-zA-Z]')
+
     Totallist = []
     Faillist = []
 
     for i in range(len(listlize2)):
         line = listlize2[i]
-        result_1 = re.sub('["'"'"'\t\n=+/;\[\]"("")"""""?:$\\\/,*}a-df-mo-rt-vx-zA-DF-MO-RT-VX-XZ]', ' ', str(line))
+        result_1 = re.sub('["'"'"'\t\n=+/;\[\]"("")"""""?_:$\\\/,*}a-df-mo-rt-vx-zA-DF-MO-RT-VX-XZ]', ' ', str(line))
         # print(result_1)
         result = result_1.replace('-', ' ').replace('N', ' N ').replace('S', ' S ').replace('n', ' N ').replace('s', ' S ').replace('W', ' W ').replace('E', ' E ').replace('w', ' W ').replace('e',' E ')
         # print(result_list)
@@ -51,11 +45,8 @@ def new_page(request):
         list_location_num = list(map(float, location_num3))
         # print(list_location_num)
         list_location_num_d = list(round(x, 1) for x in list_location_num)  # 방향제외 좌표완성
-        # print(list_location_num_d)
+        print(list_location_num_d)
 
-        location_s = re.sub('[0-9.-]', '', str_result_b)
-        location_s2 = ' '.join(location_s.split())
-        list_location_s = location_s2.split()
 
         # print(list_location_s)
         # print(str(list_location_s[0]).isalpha())
@@ -75,16 +66,15 @@ def new_page(request):
         if matchingcheck() and bool(is_number(str(result_b[0])) and is_number(str(result_b[1])) and str(
                 result_b[2]).replace('S', 'N') == 'N' and is_number(str(result_b[3])) and is_number(
             str(result_b[4])) and str(result_b[5]).replace('W', 'E') == 'E'):
-            final_result = str(result_b[0]).zfill(2) + '-' + str(result_b[1]).zfill(4) + \
+            final_result = str(result_b[0]).zfill(2) + '-' + str(list_location_num_d[1]).zfill(4) + \
                            result_b[
-                               2] + ' ' + str(result_b[3]).zfill(3) + '-' + str(result_b[4]).zfill(4) + \
+                               2] + ' ' + str(result_b[3]).zfill(3) + '-' + str(list_location_num_d[3]).zfill(4) + \
                            result_b[5]
 
             Totallist.append(final_result)
 
             # print(final_result)
 
-        # below code is checking for matching
 
         else:
             Faillist.append(line + "<<------- Couldn't be converted")
@@ -97,7 +87,11 @@ def new_page(request):
 
     return render(request, 'blog/post_list.html', {'data': data, 'Faillist':Faillist_data})
 
-
+'''
+1. 분이 60을 넘겼을시 도로 환산하는 계산식이 필요함
+2. Coppy button
+3. 자동 GC 입력 (일정 NM을 넘겼을시)
+'''
 
 
 
