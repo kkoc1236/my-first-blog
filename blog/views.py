d0c3fad62f03662d531b70facecab4619e99f566
Your Name <you@example.com>
2019-07-19 14:56:09 +0900

    0719final

--- blog/views.py
+++ blog/views.py
@@ -27,15 +27,13 @@ def new_page(request):
 
     for i in range(len(listlize2)):
         line = listlize2[i]
-        result_1 = re.sub('["'"'"'\t\n=+/;\[\]"("")"""""?><_:$\\\/,*}a-df-mo-rt-vx-zA-DF-MO-RT-VX-XZ"属"]', ' ', str(line))
-        # print(result_1)
-        result = result_1.replace('-', ' ').replace('N', ' N ').replace('S', ' S ').replace('n', ' N ').replace('s', ' S ').replace('W', ' W ').replace('E', ' E ').replace('w', ' W ').replace('e',' E ')
-        # print(result_list)
-        result_a = " ".join(result.split())
-        result_f = result_a.split()  #### ['00', '00.0', 'N', '00', '00.0', 'N']
+        lineup = line.replace('n', 'N').replace('s', 'S').replace('w', 'W').replace('e', 'E')
+        result_f = re.findall(r"[-+]?\d*\.\d+|\d+|E|W|N|S|e|w|n|s", str(lineup))
+        print(result_f)
+
         result_b = result_f[-6:]
         str_result_b = ' '.join(result_b)
-        print('aaa', result_b)
+        #print('aaa', result_b)
         result2 = re.sub('[a-zA-Z]', '', str_result_b)
         location_num = result2.split(' ')
         location_num1 = ' '.join(location_num)
@@ -45,7 +43,7 @@ def new_page(request):
         list_location_num = list(map(float, location_num3))
         # print(list_location_num)
         list_location_num_d = list(round(x, 1) for x in list_location_num)  # 방향제외 좌표완성
-        print(list_location_num_d)
+        #print(list_location_num_d)
 
 
         # print(list_location_s)
@@ -62,7 +60,7 @@ def new_page(request):
             except IndexError:  # num을 float으로 변환할 수 없는 경우
                 return False
 
-        print('xxx', matchingcheck())
+        #print('xxx', matchingcheck())
         if matchingcheck() and bool(is_number(str(result_b[0])) and is_number(str(result_b[1])) and str(
                 result_b[2]).replace('S', 'N') == 'N' and is_number(str(result_b[3])) and is_number(
             str(result_b[4])) and str(result_b[5]).replace('W', 'E') == 'E'):
@@ -92,9 +90,7 @@ def new_page(request):
 2. Coppy button
 3. 뒤에서부터 읽기 [x:] 개선해 보기
 4. 자동 GC 입력 (일정 NM을 넘겼을시)
-5. result_1 = re.sub('["'"'"'\t\n=+/;\[\]"("")"""""?><_:$\\\/,*}a-df-mo-rt-vx-zA-DF-MO-RT-VX-XZ"属"]', ' ', str(line)) 부분을 
-re.findall(r"[-+]?\d*\.\d+|\d+|E|W|N|S|e|w|n|s", 대상)로 대체 가능할듯..
-6. 반본에서 if문에 break 를 사용하면 반복을 중간에 멈출 수 있다
+6. 반복에서 if문에 break 를 사용하면 반복을 중간에 멈출 수 있다
 '''
 
 
